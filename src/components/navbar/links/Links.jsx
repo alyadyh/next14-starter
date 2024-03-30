88b2459b3bc1b3132f342c047174015cd4143db1
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-03-30 13:05:52 +0700

    Set active class to navbar

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import styles from "./links.module.css"
+import NavLink from "./navLink/navLink";
 
 const Links = () => {
 
@@ -22,11 +22,23 @@ const Links = () => {
         },
     ];
 
+    // TEMPORARY
+    const session = true;
+    const isAdmin = true;
+
   return (
     <div className={styles.links}>
-        {links.map((link => (
-            <Link href={link.path} key={link.title}>{link.title}</Link>
-        )))}
+        {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }}/> }
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
     </div>
   )
 }
