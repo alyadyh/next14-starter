4246483376eccdff4e37885b419ed79d4d5a6fc5
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-04-01 01:51:11 +0700

    Set SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,25 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/navbar/Navbar'
-import Footer from '@/components/footer/Footer'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 // import ClientSideProviderTest from "@/components/clientSideProviderTest";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Next App',
-  description: 'Next.js starter app',
-}
+  title: {
+    default:"Next.js 14 Homepage",
+    template:"%s | Next.js 14"
+  },
+  description: "Next.js starter app description",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         {/* <ClientSideProviderTest> */}
-        <div className="container">
+          <div className="container">
             <Navbar />
             {children}
             <Footer />
@@ -24,5 +27,5 @@ export default function RootLayout({ children }) {
         {/* </ClientSideProviderTest> */}
       </body>
     </html>
-  )
+  );
 }
