4048ac7b19afafe2faf7371d62310fcaf18e8d72
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-04-04 20:56:11 +0700

    Auth middleware

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['i.pinimg.com'],
+      protocol: 'https',
+      hostname: ['i.pinimg.com'],
     },
 }
 
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -9,11 +9,11 @@ const PostCard = ({post}) => {
         {post.img && <div className={styles.imgContainer}>
           <Image src={post.img} alt="" fill className={styles.img}/>
         </div>}
-        <span className={styles.date}>01.01.2021</span>
+        <span className={styles.date}>{post.createdAt?.toString().slice(4, 16)}</span>
       </div>
       <div className={styles.bottom}>
-      <h1 className={styles.title}>{post.title}</h1>
-        <p className={styles.desc}>{post.desc}</p>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
         <Link className={styles.link} href={`/blog/${post.slug}`}>READ MORE</Link>
       </div>
     </div>
